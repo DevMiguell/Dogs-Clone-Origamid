313eb2ed3b750331434233a9ec1539c0e6ae7a67
DevMiguell <buguel2000@gmail.com>
2021-03-25 14:05:51 -0300

    criando o useFetch e o cadastrar

--- src/Hooks/useForm.js
+++ src/Hooks/useForm.js
@@ -5,6 +5,10 @@ const types = {
     regex: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     message: 'Preencha um email válido',
   },
+  password: {
+    regex: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+    message: 'A senha deve conter no mínimo de oito caracteres, pelo menos uma letra e um número',
+  }
 };
 
 const useForm = (type) => {
